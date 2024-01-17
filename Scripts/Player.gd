e5a681d914e6182201292d3d3a21b456085036cb
Martin Kučera <xkucer0s@vutbr.cz>
2024-01-17 15:24:41 +0100

    Created Godot project + added player movement

---
+++ Scripts/Player.gd
@@ -0,0 +1,7 @@
+extends CharacterBody2D
+
+
+func _physics_process(_delta):
+	var direction = Input.get_vector("moveLeft", "moveRight", "moveUp", "moveDown")
+	velocity = direction * 200
+	move_and_slide()
